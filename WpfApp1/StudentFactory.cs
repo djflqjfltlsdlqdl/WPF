3258213d18e068d201bd09c3e2f7fb6f148827f3
praytop <praytop@gmail.com>
2020-08-26 00:17:08 +0900

    Project Initialize

---
+++ WpfApp1/StudentFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class StudentFactory
+    {
+        private IList<Student> students;
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return students;
+        }
+
+        public StudentFactory()
+        {
+            students = new List<Student>();
+
+            students.Add(new Student() { Grade = "1", Cclass = "3", Name = "gildong", No = "1010", Score = "A" });
+            students.Add(new Student() { Grade = "1", Cclass = "1", Name = "gildong", No = "1020", Score = "A" });
+            students.Add(new Student() { Grade = "1", Cclass = "2", Name = "gildong", No = "1030", Score = "A" });
+            students.Add(new Student() { Grade = "1", Cclass = "4", Name = "gildong", No = "1040", Score = "B" });
+            students.Add(new Student() { Grade = "1", Cclass = "2", Name = "gildong", No = "1050", Score = "A" });
+            students.Add(new Student() { Grade = "1", Cclass = "5", Name = "gildong", No = "1060", Score = "C" });
+        }
+    }
+
+    public class Student : Notifier
+    {
+        private string grade;
+        public string Grade
+        {
+            get { return grade; }
+            set
+            {
+                grade = value; OnPropertyChanged("Grade");
+            }
+        }
+        private string cclass;
+        public string Cclass
+        {
+            get { return cclass; }
+            set
+            {
+                cclass = value; OnPropertyChanged("Cclass");
+            }
+        }
+        private string no;
+        public string No
+        {
+            get { return no; }
+            set
+            {
+                no = value; OnPropertyChanged("No");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value; OnPropertyChanged("Name");
+            }
+        }
+        private string score;
+        public string Score
+        {
+            get { return score; }
+            set
+            {
+                score = value; OnPropertyChanged("Score");
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    public class StudentViewModel : Notifier
+    {
+        //데이터 불러올 생성자 생성
+        StudentFactory factory = new StudentFactory();
+
+        // view 단에서 쓸 모델명
+        private IEnumerable<Student> foundStudents;
+        public IEnumerable<Student> FoundStudents
+        {
+            get { return foundStudents; }
+            set
+            {
+                foundStudents = value; OnPropertyChanged("FoundStudents");
+            }
+        }
+        // view 단에서 쓸 모델명
+        private Student selectedStudent;
+        public Student SelectedStudent
+        {
+            get { return selectedStudent; }
+            set
+            {
+                selectedStudent = value; OnPropertyChanged("SelectedStudent");
+            }
+        }
+        public StudentViewModel()
+        {
+        }
+        //Read에서 사용할 이벤트 커맨드 생성
+        private ICommand readCommand;
+        public ICommand ReadCommand
+        {
+            get { return (this.readCommand) ?? (this.readCommand = new DelegateCommand(Read)); }
+        }
+        private void Read()
+        {
+            FoundStudents = factory.GetAllStudent();
+        }
+    }
+    public class DelegateCommand : ICommand
+    {
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        public DelegateCommand(Action excute) : this(excute, null)
+        {
+
+        }
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object o)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        public void Execute(object o)
+        {
+            this.execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if(this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
